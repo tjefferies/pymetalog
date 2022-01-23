433afd1493c081036a97e05ec3e1220f051245b4
Gregor Boehl <admin@gregorboehl.com>
2022-01-23 11:19:38 +0100

    add dependencies

--- setup.py
+++ setup.py
@@ -17,4 +17,10 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scipy',
+        'seaborn',
+     ],
 )
